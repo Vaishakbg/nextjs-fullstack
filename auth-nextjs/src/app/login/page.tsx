addc9936da87b7f0cc5fb1752a63682907c27821
Vaishak <vaishakbg@gmail.com>
2023-12-28 14:40:17 +0000

    auth-nextjs --- Login auth store cookie

--- auth-nextjs/src/app/login/page.tsx
+++ auth-nextjs/src/app/login/page.tsx
@@ -3,20 +3,43 @@ import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
     });
-
+    const [buttonDisabled, setButtonDisabled] = React.useState(false)
+    const [isLoading, setIsLoading] = React.useState(false)
     const onLogin = async () => {
+      try{
+        setIsLoading(true);
+        const response = await axios.post("/api/users/login", user);
+        console.log("Login Success", response.data);
+        toast.success("Login success");
+        router.push('/profile')
+      } catch (error: any) {
+        console.log("Login failed", error.message)
+        toast.error(error.message)
 
+      } finally {
+        setIsLoading(false);
+      }
     }
+
+    React.useEffect(()=>{
+      if(user.email.length > 0 && user.password.length > 0) {
+        setButtonDisabled(false)
+      } else {
+        setButtonDisabled(true)
+      }
+    }, [user])
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Login</h1>
+            <h1>{isLoading ? 'Processing...' : 'Login'}</h1>
             <hr />
 
             <label htmlFor="email">Email</label>
@@ -39,7 +62,8 @@ export default function LoginPage() {
             />
             <button
               onClick={onLogin}
-              className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
+              className={`p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 ${buttonDisabled ? 'opacity-50':''}`}
+              disabled={buttonDisabled}>Login here</button>
             <Link href="/signup"> Visit signup page </Link>
         </div>
     )
