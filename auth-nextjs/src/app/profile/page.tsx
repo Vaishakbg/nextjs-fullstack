9440790462eba84c22710f97008d10aaf306eb33
Vaishakbg <vaishakbg@gmail.com>
2023-12-29 18:26:12 +0530

    auth-nextjs --- Extract data from token

--- auth-nextjs/src/app/profile/page.tsx
+++ auth-nextjs/src/app/profile/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import axios from "axios"
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
     const router = useRouter();
+    const [data, setData] = useState();
     const logout = async () => {
         try {
             await axios.get("/api/users/logout");
@@ -18,13 +21,23 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get("/api/users/me");
+        console.log(res.data);
+        setData(res.data.data._id);
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
+            <h2 className="bg-purple-400 text-white p-2 rounded">{data ? <Link href={`/profile/${data}`}>Me</Link> : 'Nothing'}</h2>
             <hr />
             <button 
               onClick={logout}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-4 py-2 px-4 rounded">Logout</button>
+              <button 
+              onClick={getUserDetails}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold mt-4 py-2 px-4 rounded">Get user details</button>
         </div>
     )
 }
