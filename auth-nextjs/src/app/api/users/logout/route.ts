5d5bdfa5218f6e5250696ae9b8ad612fe44509a7
Vaishakbg <vaishakbg@gmail.com>
2023-12-29 17:51:58 +0530

    auth-nextjs --- logout functionality

---
+++ auth-nextjs/src/app/api/users/logout/route.ts
@@ -0,0 +1,19 @@
+import { connect } from "@/dbConfig/dbConfig";
+import {NextResponse} from "next/server";
+
+connect();
+
+export async function GET() {
+    try {
+        const response = NextResponse.json(
+            {
+                messsage: "Logout successful",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0)});
+        return response;
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
